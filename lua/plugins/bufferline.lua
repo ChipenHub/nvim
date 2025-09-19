c5983479b98521435210895641fd0d1751f8048d
Chipen <chipen@mail.com>
2025-09-19 23:00:25 +0800

    bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,8 +4,8 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {},
     keys = {
-        { "<C-i>",  "<cmd>BufferLineCyclePrev<CR>", silent = true, noremap = true, desc = "Prev Buffer" },
-        { "<C-o>", "<cmd>BufferLineCycleNext<CR>", silent = true, noremap = true, desc = "Next Buffer" },
+        { "<Tab>",  "<cmd>BufferLineCyclePrev<CR>", silent = true, noremap = true, desc = "Prev Buffer" },
+        { "<leader><Tab>", "<cmd>BufferLineCycleNext<CR>", silent = true, noremap = true, desc = "Next Buffer" },
         { "gp",    "<cmd>BufferLinePick<CR>",      silent = true, noremap = true, desc = "Pick Buffer" },
         -- 关闭当前 buffer
         { "<C-\\>", "<cmd>bdelete<CR>",             silent = true, noremap = true, desc = "Close Buffer" },
